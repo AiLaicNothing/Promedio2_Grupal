992abb1409dd59f6e976a1b90da2424e39765be1
Lord Rum <116317848+LordRumTheThird@users.noreply.github.com>
2026-06-04 19:11:12 -0500

    HelicopterDone

--- Grupal2/Assets/AlvaSus/Helicopter.cs
+++ Grupal2/Assets/AlvaSus/Helicopter.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,13 +7,17 @@ public class Helicopter : EnemyBase
     GameObject[] PlayerTeam;
     GameObject target;
     NavMeshAgent agent;
+    [SerializeField] GameObject Bullet;
+    [SerializeField] GameObject shootPoint;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] int speed=5;
     public void ChooseTarget()
     {
         PlayerTeam = GameObject.FindGameObjectsWithTag("Player");
         GameObject choose = PlayerTeam[0];
         int position = 0;
 
-        foreach (GameObject player in PlayerTeam)
+        foreach (GameObject go in PlayerTeam)
         {
             float targetDistance = Vector3.Distance(transform.position, choose.transform.position);
             float thisDistance = Vector3.Distance(transform.position, PlayerTeam[position].transform.position);
@@ -34,9 +39,35 @@ public class Helicopter : EnemyBase
     {
         ChooseTarget();
         Move();
+        Shooting();
     }
     private void Move()
     {
         agent.destination = target.transform.position;
+    }
+    private void Shooting()
+    {
+        PlayerTeam = GameObject.FindGameObjectsWithTag("Player");
+        GameObject target = PlayerTeam[0];
+        int position = 0;
+
+        foreach (GameObject go in PlayerTeam)
+        {
+            float targetDistance = Vector3.Distance(transform.position, target.transform.position);
+            float thisDistance = Vector3.Distance(transform.position, PlayerTeam[position].transform.position);
+            if (targetDistance > thisDistance)
+            {
+                target = PlayerTeam[position];
+            }
+            position++;
+        }
+        transform.LookAt(target.transform);
+        Shoot();
+    }
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, shootPoint.transform.position, Quaternion.identity);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.forward * speed;
     }
 }
